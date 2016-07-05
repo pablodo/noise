d2f4a72ee2b2543619f05f213cf748133b97fb44
Pablo Díaz Ogni <pablo@jampp.com>
2016-07-05 13:06:57 -0300

    Count words by default

--- noise.py
+++ noise.py
@@ -1,14 +1,17 @@
 import re
 from collections import defaultdict
 
-regex = re.compile("\d+\/\d+\/\d+, \d+:\d+ - (.*?):")
-def main(input_file):
+regex = re.compile("\d+\/\d+\/\d+, \d+:\d+ - (.*?): (.*)")
+def main(input_file, count_type):
     users = defaultdict(int)
     for line in input_file:
         match = regex.search(line)
         if match:
-            user = match.groups()[0]
-            users[user] += 1
+            user, message = match.groups()
+            if count_type == 'words':
+                users[user] += len(message.split(' '))
+            elif count_type == 'messages':
+                users[user] += 1
 
     for user, count in users.iteritems():
         print "%s: %s" % (user, count)
@@ -18,6 +21,7 @@ if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument('input_file', type=argparse.FileType('r'))
+    ap.add_argument('--count_type', '-t', choices=['words', 'messages'], default='words')
     args = ap.parse_args()
 
-    main(args.input_file)
+    main(args.input_file, args.count_type)
