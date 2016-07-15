8318e7891a9db603fc5ac3290f1dfb39a286a02c
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2016-07-15 00:26:38 -0300

    Add count by chars option

--- noise.py
+++ noise.py
@@ -2,7 +2,7 @@ import re
 from collections import defaultdict
 
 regex = re.compile("\d+\/\d+\/\d+, \d+:\d+ - (.*?): (.*)")
-def main(input_file, count_type):
+def main(input_file, count_type, separator):
     users = defaultdict(int)
     for line in input_file:
         match = regex.search(line)
@@ -12,16 +12,19 @@ def main(input_file, count_type):
                 users[user] += len(message.split(' '))
             elif count_type == 'messages':
                 users[user] += 1
+            elif count_type == 'chars':
+                users[user] += sum([len(x) for x in message.split(' ')])
 
     for user, count in users.iteritems():
-        print "%s: %s" % (user, count)
+        print "%s%s%s" % (user, separator, count)
 
 
 if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument('input_file', type=argparse.FileType('r'))
-    ap.add_argument('--count_type', '-t', choices=['words', 'messages'], default='words')
+    ap.add_argument('--count_type', '-t', choices=['words', 'messages', 'chars'], default='words')
+    ap.add_argument('--separator', '-s', default=',')
     args = ap.parse_args()
 
-    main(args.input_file, args.count_type)
+    main(args.input_file, args.count_type, args.separator)
